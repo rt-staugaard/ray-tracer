735725227d4515ee3c3aea26a71560ba2ee8b54a
rstaugaard <qjm549@alumni.ku.dk>
2026-05-06 13:31:56 +0200

    cleaned up and added build through cmake

---
+++ build/CMakeFiles/ray-tracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray-tracer.dir/version/new/main.cpp.o"
+  "CMakeFiles/ray-tracer.dir/version/new/main.cpp.o.d"
+  "ray-tracer"
+  "ray-tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray-tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ray-tracer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ray-tracer.
+# This may be replaced when dependencies are built.
